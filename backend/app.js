56cb12707de4b5c0e13cccb7feaca33480d06525
ahmed mhmoud abdelaal <ahmedmhmouad41@gmail.com>
2025-05-02 14:41:25 +0300

    Configure Express application with middleware and routes

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const errorHandler = require('./middleware/error');
 const userRoutes = require('./modules/users/users.routes.js');
+const courseRoutes = require('./modules/courses/courses.routes.js');
 // Add other routes
 
 const app = express();
 app.use(express.json());
 app.use('/api/users', userRoutes);
+app.use('/api/courses', courseRoutes);
 // Add other routes
 app.use(errorHandler);
 
