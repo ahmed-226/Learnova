232b1cdc5a857fc795aadb63163551ef798588a3
ahmed mhmoud abdelaal <ahmedmhmouad41@gmail.com>
2025-05-03 18:17:19 +0300

    Update app.js with new route registrations

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,18 @@
-// Express app setup
 const express = require('express');
 const errorHandler = require('./middleware/error');
 const userRoutes = require('./modules/users/users.routes.js');
 const courseRoutes = require('./modules/courses/courses.routes.js');
-// Add other routes
+const lessonsRoutes = require('./modules/lessons/lessons.routes');
+const quizzesRoutes = require('./modules/quizzes/quizzes.routes.js');
+
 
 const app = express();
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/courses', courseRoutes);
-// Add other routes
+app.use('/api/lessons', lessonsRoutes);
+app.use('/api/quizzes', quizzesRoutes); 
+
 app.use(errorHandler);
 
 module.exports = app;
