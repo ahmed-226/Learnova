1e7081e6e29b014da548153f7d17d91209e4402f
ahmed mhmoud abdelaal <ahmedmhmouad41@gmail.com>
2025-05-08 03:49:02 +0300

    Implement assignment management methods in forums controller

--- backend/modules/forums/forums.controller.js
+++ backend/modules/forums/forums.controller.js
@@ -1,4 +1,69 @@
-// Forums controller
-module.exports = {
-  // Add controller methods here
+const assignmentService = require('./assignments.service');
+const logger = require('../../utils/logger');
+const { HTTP_STATUS } = require('../../utils/constants');
+
+
+const createAssignment = async (req, res, next) => {
+  try {
+    const instructorId = req.user.id;
+    const assignment = await assignmentService.createAssignment(req.body, instructorId);
+    
+    logger.info(`Assignment created: ${assignment.id} by instructor ${instructorId}`);
+    res.status(HTTP_STATUS.CREATED).json(assignment);
+  } catch (error) {
+    logger.error(`Error in createAssignment: ${error.message}`);
+    next(error);
+  }
 };
+
+
+const getAssignmentById = async (req, res, next) => {
+  try {
+    const assignmentId = req.params.assignmentId;
+    
+    const assignment = await assignmentService.getAssignmentById(assignmentId);
+    res.status(HTTP_STATUS.OK).json(assignment);
+  } catch (error) {
+    logger.error(`Error in getAssignmentById: ${error.message}`);
+    next(error);
+  }
+};
+
+
+const updateAssignment = async (req, res, next) => {
+  try {
+    const assignmentId = req.params.assignmentId;
+    const instructorId = req.user.id;
+    
+    const updatedAssignment = await assignmentService.updateAssignment(assignmentId, req.body, instructorId);
+    
+    logger.info(`Assignment updated: ${assignmentId} by instructor ${instructorId}`);
+    res.status(HTTP_STATUS.OK).json(updatedAssignment);
+  } catch (error) {
+    logger.error(`Error in updateAssignment: ${error.message}`);
+    next(error);
+  }
+};
+
+
+const deleteAssignment = async (req, res, next) => {
+  try {
+    const assignmentId = req.params.assignmentId;
+    const instructorId = req.user.id;
+    
+    await assignmentService.deleteAssignment(assignmentId, instructorId);
+    
+    logger.info(`Assignment deleted: ${assignmentId} by instructor ${instructorId}`);
+    res.status(HTTP_STATUS.OK).json({ success: true });
+  } catch (error) {
+    logger.error(`Error in deleteAssignment: ${error.message}`);
+    next(error);
+  }
+};
+
+module.exports={
+  createAssignment,
+  getAssignmentById,
+  updateAssignment,
+  deleteAssignment
+}
