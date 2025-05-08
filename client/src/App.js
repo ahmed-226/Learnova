07b43599406e412a8214c8865feb3a0d832c3d24
ahmed mhmoud abdelaal <ahmedmhmouad41@gmail.com>
2025-05-09 00:47:47 +0300

    feat: update routing structure in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import HomePage from './components/HomePage.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/home/HomePage.jsx';
+import AuthPage from './pages/auth/AuthPage.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
