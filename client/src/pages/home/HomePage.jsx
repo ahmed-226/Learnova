334acb84500a47ff191040bed1df5f1df5ca6e8c
ahmed mhmoud abdelaal <ahmedmhmouad41@gmail.com>
2025-05-09 00:45:34 +0300

    feat: create HomePage component with layout structure

---
+++ client/src/pages/home/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from '../../components/layout/Navbar';
+import Footer from '../../components/layout/Footer';
+import HeroSection from '../../components/home/HeroSection';
+import StatsSection from '../../components/home/StatsSection';
+import FeatureSection from '../../components/home/FeatureSection';
+import CoursesSection from '../../components/home/CoursesSection';
+import TestimonialsSection from '../../components/home/TestimonialsSection';
+import CTASection from '../../components/home/CTASection';
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <HeroSection />
+      <StatsSection />
+      <FeatureSection />
+      <CoursesSection />
+      <TestimonialsSection />
+      <CTASection />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
