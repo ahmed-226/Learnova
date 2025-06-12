b6f55a575b6132eddeeaad94fd0ec739cda6c37a
ahmed mhmoud abdelaal <ahmedmhmouad41@gmail.com>
2025-06-13 02:20:36 +0300

    feat: wrap application in AuthProvider and implement ProtectedRoute for route access control

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 import HomePage from './pages/HomePage.jsx';
 import AuthPage from './pages/AuthPage.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
@@ -18,31 +19,67 @@ import CourseModuleManagementPage from './pages/CourseModuleManagementPage.jsx';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            {/* PUBLIC ROUTES - Anyone can access */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/courses" element={<CoursesPage />} />
+            <Route path="/courses/:courseId" element={<CourseDetailsPage />} />
 
-          <Route path="/courses" element={<CoursesPage />} />
-          <Route path="/courses/:courseId" element={<CourseDetailsPage />} />
-          <Route path="/courses/:courseId/:contentType/:contentId" element={<CourseContentPage />} />
-          
-          <Route path="/courses/:courseId/forum" element={<ForumPage />} />
-          <Route path="/courses/:courseId/forum/thread/:threadId" element={<ThreadPage />} />
-          
-          <Route path="/create-course" element={<CreateCoursePage />} />
-          <Route path="/courses/:courseId/modules" element={<CourseModuleManagementPage />} />
+            {/* PROTECTED ROUTES - Only logged-in users */}
+            <Route path="/profile" element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            } />
+            
+            <Route path="/dashboard" element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            } />
 
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </div>
-    </Router>
+            <Route path="/courses/:courseId/:contentType/:contentId" element={
+              <ProtectedRoute>
+                <CourseContentPage />
+              </ProtectedRoute>
+            } />
+            
+            <Route path="/courses/:courseId/forum" element={
+              <ProtectedRoute>
+                <ForumPage />
+              </ProtectedRoute>
+            } />
+            
+            <Route path="/courses/:courseId/forum/thread/:threadId" element={
+              <ProtectedRoute>
+                <ThreadPage />
+              </ProtectedRoute>
+            } />
+
+            {/* INSTRUCTOR-ONLY ROUTES - Only instructors can access */}
+            <Route path="/create-course" element={
+              <ProtectedRoute requiredRole="INSTRUCTOR">
+                <CreateCoursePage />
+              </ProtectedRoute>
+            } />
+            
+            <Route path="/courses/:courseId/modules" element={
+              <ProtectedRoute requiredRole="INSTRUCTOR">
+                <CourseModuleManagementPage />
+              </ProtectedRoute>
+            } />
+
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
